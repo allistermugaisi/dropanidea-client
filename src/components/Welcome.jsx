a34bb2187d7de32d88e4a6b22ce59a78866be61f
allistermugaisi <allistermugaisi@gmail.com>
2022-01-11 06:37:55 +0300

    added welcome screen

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../public/css/Welcome.css';
 
 const Welcome = () => {
 	return (
-		<div>
-			<h2>Complete psychometric test on signup</h2>
-			<Link to="dashboard">Proceed to dashboard</Link>
+		<div className="welcome-container">
+			<div className="logo-welcome-container">
+				<img
+					src="https://res.cloudinary.com/yugillc/image/upload/q_auto/v1641840407/logoz-trans_2_uuslx1.png"
+					className="logo-land"
+					alt=""
+				/>
+			</div>
+			<div className="btn-container">
+				<Link to="dashboard" className="btn-landing">
+					Continue
+				</Link>
+			</div>
 		</div>
 	);
 };
